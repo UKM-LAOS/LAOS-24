059d68c8c7c866821a45029df2fc94cd65669a68
ChristYoga123 <christianuswibisono@gmail.com>
2024-10-25 21:31:00 +0700

    feat: copying assets

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/web/course/frontpage.php';
+require __DIR__.'/web/laos-course/frontpage.php';
 
 Route::get('/', function () {
     return view('welcome');
--- routes/web/laos-course/frontpage.php
+++ routes/web/laos-course/frontpage.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/course', function()
 {
-    return 'Course Frontpage';
+    return view('laos-course.layouts.guest');
 });
 
 ?>
